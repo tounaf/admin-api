5d4698e2c0242a841220b90dd6425d3831cd767e
Fetraharinjatovo <fetra.harinjatovo@pulse.mg>
2025-07-03 16:56:20 +0300

    offering

---
+++ migrations/Version20250703132808.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250703132808 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            CREATE TABLE offering (id INT AUTO_INCREMENT NOT NULL, type VARCHAR(50) NOT NULL, quantities JSON DEFAULT NULL COMMENT '(DC2Type:json)', total DOUBLE PRECISION NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            DROP TABLE offering
+        SQL);
+    }
+}
